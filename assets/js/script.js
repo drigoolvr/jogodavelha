e00fb138b22417545418030a8b71e285275a421d
Rodrigo Oliveira <drigo_olvr@hotmail.com>
2020-09-17 17:42:45 -0300

    Ajustes na base do jogo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,138 +1,67 @@
 //Quadrado 1
 document.querySelector('.quadrado1').addEventListener("click", function(){
-    let player = document.querySelector('.jogador').innerHTML;
-    if(player == "O") {
-        document.querySelector('.quadrado1').innerHTML = "O";
-        document.querySelector('.jogador').innerHTML = "X";
-    }
-    else {
-        document.querySelector('.quadrado1').innerHTML = "X";
-        document.querySelector('.jogador').innerHTML = "O";
-    }
-
-    endgame();    
+    let quad = document.querySelector('.quadrado1');    
+    jogada(quad);       
 });
 
 //Quadrado 2
 document.querySelector('.quadrado2').addEventListener("click", function(){
-    let player = document.querySelector('.jogador').innerHTML;
-    if(player == "O") {
-        document.querySelector('.quadrado2').innerHTML = "O";
-        document.querySelector('.jogador').innerHTML = "X";
-    }
-    else {
-        document.querySelector('.quadrado2').innerHTML = "X";
-        document.querySelector('.jogador').innerHTML = "O";
-    }
-
-    endgame();    
+    let quad = document.querySelector('.quadrado2');
+    jogada(quad); 
 });
 
 //Quadrado 3
-document.querySelector('.quadrado3').addEventListener("click", function(){
-    let player = document.querySelector('.jogador').innerHTML;
-    if(player == "O") {
-        document.querySelector('.quadrado3').innerHTML = "O";
-        document.querySelector('.jogador').innerHTML = "X";
-    }
-    else {
-        document.querySelector('.quadrado3').innerHTML = "X";
-        document.querySelector('.jogador').innerHTML = "O";
-    }
-
-    endgame();    
+document.querySelector('.quadrado3').addEventListener("click", function(){   
+    let quad = document.querySelector('.quadrado3');
+    jogada(quad); 
 });
 
 //Quadrado 4
 document.querySelector('.quadrado4').addEventListener("click", function(){
-    let player = document.querySelector('.jogador').innerHTML;
-    if(player == "O") {
-        document.querySelector('.quadrado4').innerHTML = "O";
-        document.querySelector('.jogador').innerHTML = "X";
-    }
-    else {
-        document.querySelector('.quadrado4').innerHTML = "X";
-        document.querySelector('.jogador').innerHTML = "O";
-    }
-
-    endgame();    
+    let quad = document.querySelector('.quadrado4');
+    jogada(quad); 
 });
 
 //Quadrado 5
 document.querySelector('.quadrado5').addEventListener("click", function(){
-    let player = document.querySelector('.jogador').innerHTML;
-    if(player == "O") {
-        document.querySelector('.quadrado5').innerHTML = "O";
-        document.querySelector('.jogador').innerHTML = "X";
-    }
-    else {
-        document.querySelector('.quadrado5').innerHTML = "X";
-        document.querySelector('.jogador').innerHTML = "O";
-    }
-
-    endgame();    
+    let quad = document.querySelector('.quadrado5');
+    jogada(quad); 
 });
 
 //Quadrado 6
 document.querySelector('.quadrado6').addEventListener("click", function(){
-    let player = document.querySelector('.jogador').innerHTML;
-    if(player == "O") {
-        document.querySelector('.quadrado6').innerHTML = "O";
-        document.querySelector('.jogador').innerHTML = "X";
-    }
-    else {
-        document.querySelector('.quadrado6').innerHTML = "X";
-        document.querySelector('.jogador').innerHTML = "O";
-    }
-
-    endgame();    
+    let quad = document.querySelector('.quadrado6');
+    jogada(quad); 
 });
 
 //Quadrado 7
 document.querySelector('.quadrado7').addEventListener("click", function(){
-    let player = document.querySelector('.jogador').innerHTML;
-    if(player == "O") {
-        document.querySelector('.quadrado7').innerHTML = "O";
-        document.querySelector('.jogador').innerHTML = "X";
-    }
-    else {
-        document.querySelector('.quadrado7').innerHTML = "X";
-        document.querySelector('.jogador').innerHTML = "O";
-    }
-
-    endgame();    
+    let quad = document.querySelector('.quadrado7');
+    jogada(quad); 
 });
 
 //Quadrado 8
 document.querySelector('.quadrado8').addEventListener("click", function(){
-    let player = document.querySelector('.jogador').innerHTML;
-    if(player == "O") {
-        document.querySelector('.quadrado8').innerHTML = "O";
-        document.querySelector('.jogador').innerHTML = "X";
-    }
-    else {
-        document.querySelector('.quadrado8').innerHTML = "X";
-        document.querySelector('.jogador').innerHTML = "O";
-    }
-
-    endgame();    
+    let quad = document.querySelector('.quadrado8');
+    jogada(quad); 
 });
 
 //Quadrado 9
 document.querySelector('.quadrado9').addEventListener("click", function(){
-    let player = document.querySelector('.jogador').innerHTML;
-    if(player == "O") {
-        document.querySelector('.quadrado9').innerHTML = "O";
-        document.querySelector('.jogador').innerHTML = "X";
-    }
-    else {
-        document.querySelector('.quadrado9').innerHTML = "X";
-        document.querySelector('.jogador').innerHTML = "O";
-    }
-
-    endgame();    
+    let quad = document.querySelector('.quadrado9');
+    jogada(quad); 
 });
 
-function endgame(){
-
+function jogada(quad){
+    let player = document.querySelector('.jogador');
+    if(player.innerHTML == "O") {
+        quad.innerHTML = "O";
+        player.innerHTML = "X";
+    }
+    else {
+        quad.innerHTML = "X";
+        player.innerHTML = "O";
+    }
 }
+
+
