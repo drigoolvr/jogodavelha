b87b29f1e34422e81550a4f1a84d159f5b1dd0b6
Rodrigo Oliveira <drigo_olvr@hotmail.com>
2020-09-17 16:19:05 -0300

    Base do jogo finalizada

---
+++ assets/js/script.js
@@ -0,0 +1,138 @@
+//Quadrado 1
+document.querySelector('.quadrado1').addEventListener("click", function(){
+    let player = document.querySelector('.jogador').innerHTML;
+    if(player == "O") {
+        document.querySelector('.quadrado1').innerHTML = "O";
+        document.querySelector('.jogador').innerHTML = "X";
+    }
+    else {
+        document.querySelector('.quadrado1').innerHTML = "X";
+        document.querySelector('.jogador').innerHTML = "O";
+    }
+
+    endgame();    
+});
+
+//Quadrado 2
+document.querySelector('.quadrado2').addEventListener("click", function(){
+    let player = document.querySelector('.jogador').innerHTML;
+    if(player == "O") {
+        document.querySelector('.quadrado2').innerHTML = "O";
+        document.querySelector('.jogador').innerHTML = "X";
+    }
+    else {
+        document.querySelector('.quadrado2').innerHTML = "X";
+        document.querySelector('.jogador').innerHTML = "O";
+    }
+
+    endgame();    
+});
+
+//Quadrado 3
+document.querySelector('.quadrado3').addEventListener("click", function(){
+    let player = document.querySelector('.jogador').innerHTML;
+    if(player == "O") {
+        document.querySelector('.quadrado3').innerHTML = "O";
+        document.querySelector('.jogador').innerHTML = "X";
+    }
+    else {
+        document.querySelector('.quadrado3').innerHTML = "X";
+        document.querySelector('.jogador').innerHTML = "O";
+    }
+
+    endgame();    
+});
+
+//Quadrado 4
+document.querySelector('.quadrado4').addEventListener("click", function(){
+    let player = document.querySelector('.jogador').innerHTML;
+    if(player == "O") {
+        document.querySelector('.quadrado4').innerHTML = "O";
+        document.querySelector('.jogador').innerHTML = "X";
+    }
+    else {
+        document.querySelector('.quadrado4').innerHTML = "X";
+        document.querySelector('.jogador').innerHTML = "O";
+    }
+
+    endgame();    
+});
+
+//Quadrado 5
+document.querySelector('.quadrado5').addEventListener("click", function(){
+    let player = document.querySelector('.jogador').innerHTML;
+    if(player == "O") {
+        document.querySelector('.quadrado5').innerHTML = "O";
+        document.querySelector('.jogador').innerHTML = "X";
+    }
+    else {
+        document.querySelector('.quadrado5').innerHTML = "X";
+        document.querySelector('.jogador').innerHTML = "O";
+    }
+
+    endgame();    
+});
+
+//Quadrado 6
+document.querySelector('.quadrado6').addEventListener("click", function(){
+    let player = document.querySelector('.jogador').innerHTML;
+    if(player == "O") {
+        document.querySelector('.quadrado6').innerHTML = "O";
+        document.querySelector('.jogador').innerHTML = "X";
+    }
+    else {
+        document.querySelector('.quadrado6').innerHTML = "X";
+        document.querySelector('.jogador').innerHTML = "O";
+    }
+
+    endgame();    
+});
+
+//Quadrado 7
+document.querySelector('.quadrado7').addEventListener("click", function(){
+    let player = document.querySelector('.jogador').innerHTML;
+    if(player == "O") {
+        document.querySelector('.quadrado7').innerHTML = "O";
+        document.querySelector('.jogador').innerHTML = "X";
+    }
+    else {
+        document.querySelector('.quadrado7').innerHTML = "X";
+        document.querySelector('.jogador').innerHTML = "O";
+    }
+
+    endgame();    
+});
+
+//Quadrado 8
+document.querySelector('.quadrado8').addEventListener("click", function(){
+    let player = document.querySelector('.jogador').innerHTML;
+    if(player == "O") {
+        document.querySelector('.quadrado8').innerHTML = "O";
+        document.querySelector('.jogador').innerHTML = "X";
+    }
+    else {
+        document.querySelector('.quadrado8').innerHTML = "X";
+        document.querySelector('.jogador').innerHTML = "O";
+    }
+
+    endgame();    
+});
+
+//Quadrado 9
+document.querySelector('.quadrado9').addEventListener("click", function(){
+    let player = document.querySelector('.jogador').innerHTML;
+    if(player == "O") {
+        document.querySelector('.quadrado9').innerHTML = "O";
+        document.querySelector('.jogador').innerHTML = "X";
+    }
+    else {
+        document.querySelector('.quadrado9').innerHTML = "X";
+        document.querySelector('.jogador').innerHTML = "O";
+    }
+
+    endgame();    
+});
+
+function endgame(){
+
+}
